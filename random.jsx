b97ccdada10a93f6d6146cf1e95510d6ab840ebc
Poojabhoye <poojabhoye177@gmail.com>
2026-07-07 18:25:53 +0530

    Create Random component for testing React Routing

---
+++ random.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Random() {
+  return (
+    <div className="container mt-5">
+
+      <div className="card shadow p-4">
+
+        <h2 className="text-primary">
+          Welcome to Random Page
+        </h2>
+
+        <p>
+          This page is created only for testing React Routing.
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
